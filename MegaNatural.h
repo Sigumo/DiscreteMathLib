752ce287527aa92a68cbeb9fec01d00289fa7fc0
NDGritsay <NikitaGritsay@icloud.com>
2016-04-04 22:36:06 +0300

    now will be override of operators

--- MegaNatural.h
+++ MegaNatural.h
@@ -7,33 +7,17 @@ public:
 	MegaNatural(MegaNaturalBase &ob) : MegaNaturalBase(ob);
 	~MegaNatural() : ~MegaNaturalBase();
 
-	//Описание: сравнение натуральных чисел
-	//Возврат(1,0,-1)
-	virtual int compare(MegaNaturalBase &ob) override;
 	//Описание: проверка, является ли число 0
 	//Возврат: (1/0)
 	virtual bool isZero() override;
-	//Описание: прибавить к числу 1
-	virtual void plusOne() override;
-	//Описание: вычитание натурального числа
-	virtual void subNat(MegaNaturalBase &ob) override;
 	//Описание: умножение числа на цифру
 	virtual void multByK(int k) override;
 	//Описание: умножение числа на 10^k
 	virtual void multByTenPowK(int k) override;
-	//Описание: прибавление натурального
-	virtual void addNat(MegaNaturalBase &ob) override;
 	//Описание: вычитание натурального, умноженного на цифру
 	virtual void subNatMulK(MegaNaturalBase &ob, int k) override;
-	//Описание: умножение на натуральное
-	virtual void multByNat(MegaNaturalBase &ob) override;
 	//Описание: первая цифра делителя при делении на натуральное
 	virtual int firstDigOfDivByNat(MegaNaturalBase &ob) override;
-	//Описание: деление на натуральное
-	virtual void divByNat(MegaNaturalBase &ob) override;
-	//Описание: остаток от деления на натуральное
-	//Возврат: остаток от деления
-	virtual MegaNaturalBase &modByNat(MegaNaturalBase &ob) override;
 	//Описание: НОД натуральных
 	virtual MegaNaturalBase &gcd(MegaNaturalBase &ob) override;
 	//Описание: НОК натуральных
--- MegaNaturalBase.h
+++ MegaNaturalBase.h
@@ -15,33 +15,17 @@ public:
 	char *toString();
 	void clearFromZero();
 
-	//Описание: сравнение натуральных чисел
-	//Возврат(1,0,-1)
-	virtual int compare(MegaNaturalBase &ob) = 0;
 	//Описание: проверка, является ли число 0
 	//Возврат: (1/0)
 	virtual bool isZero() = 0;
-	//Описание: прибавить к числу 1
-	virtual void plusOne() = 0;
-	//Описание: вычитание натурального числа
-	virtual void subNat(MegaNaturalBase &ob) = 0;
 	//Описание: умножение числа на цифру
 	virtual void multByK(int k) = 0;
 	//Описание: умножение числа на 10^k
 	virtual void multByTenPowK(int k) = 0;
-	//Описание: прибавление натурального
-	virtual void addNat(MegaNaturalBase &ob) = 0;
 	//Описание: вычитание натурального, умноженного на цифру
 	virtual void subNatMulK(MegaNaturalBase &ob, int k) = 0;
-	//Описание: умножение на натуральное
-	virtual void multByNat(MegaNaturalBase &ob) = 0;
 	//Описание: первая цифра елителя при делении на натуральное
 	virtual int firstDigOfDivByNat(MegaNaturalBase &ob) = 0;
-	//Описание: деление на натуральное
-	virtual void divByNat(MegaNaturalBase &ob) = 0;
-	//Описание: остаток от деления на натуральное
-	//Возврат: остаток от деления
-	virtual MegaNaturalBase &modByNat(MegaNaturalBase &ob) = 0;
 	//Описание: НОД натуральных
 	virtual MegaNaturalBase &gcd(MegaNaturalBase &ob) = 0;
 	//Описание: НОК натуральных
