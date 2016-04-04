bea98e64df8f9587c3faa866eb382e7b81bcff48
NDGritsay <NikitaGritsay@icloud.com>
2016-04-04 23:41:14 +0300

    added Caps

--- MegaNatural.cpp
+++ MegaNatural.cpp
@@ -3,3 +3,46 @@
 MegaNatural::MegaNatural() : MegaNaturalBase::MegaNaturalBase() {}
 MegaNatural::MegaNatural(MegaNaturalBase &ob) : MegaNaturalBase::MegaNaturalBase(ob) {}
 MegaNatural::~MegaNatural() {}
+
+//Описание: проверка, является ли число 0
+//Возврат: (1/0)
+bool MegaNatural::isZero() 
+{
+	return true;
+}
+
+//Описание: умножение числа на цифру
+void MegaNatural::multByK(int k)
+{
+
+}
+
+//Описание: умножение числа на 10^k
+void MegaNatural::multByTenPowK(int k)
+{
+
+}
+
+//Описание: вычитание натурального, умноженного на цифру
+void MegaNatural::subNatMulK(MegaNaturalBase &ob, int k)
+{
+
+}
+
+//Описание: первая цифра делителя при делении на натуральное
+int MegaNatural::firstDigOfDivByNat(MegaNaturalBase &ob)
+{
+	return 0;
+}
+
+//Описание: НОД натуральных
+MegaNaturalBase &MegaNatural::gcd(MegaNaturalBase &ob)
+{
+	return ob;
+}
+
+//Описание: НОК натуральных
+MegaNaturalBase &MegaNatural::lcm(MegaNaturalBase &ob)
+{
+	return ob;
+}
--- MegaNatural.h
+++ MegaNatural.h
@@ -9,19 +9,19 @@ public:
 
 	//Описание: проверка, является ли число 0
 	//Возврат: (1/0)
-	virtual bool isZero() override;
+	bool isZero() override;
 	//Описание: умножение числа на цифру
-	virtual void multByK(int k) override;
+	void multByK(int k) override;
 	//Описание: умножение числа на 10^k
-	virtual void multByTenPowK(int k) override;
+	void multByTenPowK(int k) override;
 	//Описание: вычитание натурального, умноженного на цифру
-	virtual void subNatMulK(MegaNaturalBase &ob, int k) override;
+	void subNatMulK(MegaNaturalBase &ob, int k) override;
 	//Описание: первая цифра делителя при делении на натуральное
-	virtual int firstDigOfDivByNat(MegaNaturalBase &ob) override;
+	int firstDigOfDivByNat(MegaNaturalBase &ob) override;
 	//Описание: НОД натуральных
-	virtual MegaNaturalBase &gcd(MegaNaturalBase &ob) override;
+	MegaNaturalBase &gcd(MegaNaturalBase &ob) override;
 	//Описание: НОК натуральных
-	virtual MegaNaturalBase &lcm(MegaNaturalBase &ob) override;
+	MegaNaturalBase &lcm(MegaNaturalBase &ob) override;
 };
 
 #ifndef MEGA_NATURAL
